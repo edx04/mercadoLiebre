0900bcd411c2d71b3dcf2d3b9d5c03e476381e54
Edgar <ed04alb@gmail.com>
2021-07-06 01:04:58 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+//npm run dev
+
+const express = require('express');
+const path = require("path");
+const app = express();
+
+app.use('/static', express.static(__dirname + '/public'));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve('views/home.html'));
+
+});
+
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve('views/register.html'));
+
+});
+
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve('views/login.html'));
+
+});
+
+
+app.listen(3000, () => {
+    console.log('Servidor funcionando');
+})
